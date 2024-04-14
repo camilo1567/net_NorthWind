36c13bca1cd8bee20b77bc689568634fdadc06a5
CamiloHorta <camilo20113@hotmail.com>
2024-04-13 19:48:01 -0500

    empiezan writers

---
+++ NorthWind.ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using NorthWind.ConsoleApp.Services;
+using NorthWind.Entities.Interfaces;
+
+IUserActionWriter Writer = default;
+
+AppLogger Logger = new AppLogger(Writer);
+Logger.WriteLog("Aplicacion comenzo");
--- NorthWind.Writers/obj/Debug/net8.0/NorthWind.Writers.AssemblyInfo.cs
+++ NorthWind.Writers/obj/Debug/net8.0/NorthWind.Writers.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("NorthWind.Entities")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NorthWind.Writers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+741c8b14ee2b0eda3cb2f64dacf79796de354ca2")]
-[assembly: System.Reflection.AssemblyProductAttribute("NorthWind.Entities")]
-[assembly: System.Reflection.AssemblyTitleAttribute("NorthWind.Entities")]
+[assembly: System.Reflection.AssemblyProductAttribute("NorthWind.Writers")]
+[assembly: System.Reflection.AssemblyTitleAttribute("NorthWind.Writers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
